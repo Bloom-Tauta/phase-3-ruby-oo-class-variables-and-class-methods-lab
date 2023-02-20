3e3742e85ab32c2fff2a7e28c5b071ed516289be
Bloom <bloomtauta@gmail.com>
2023-02-21 02:28:55 +0300

    Completed assignment

---
+++ lib/song.rb
@@ -0,0 +1,60 @@
+require 'pry'
+class Song
+    attr_accessor :name, :artist, :genre
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << @artist
+        @@genres << @genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artists
+         @@artists.uniq
+    end
+
+     def self.genre_count
+      
+        genre_count = Hash.new(0) 
+        @@genres.each do |genre|
+            if genre_count[genre]
+                genre_count[genre] +=1
+            else
+                genre_count[genre] = 1
+            end
+        end
+        genre_count
+    end
+    def self.artist_count
+    artist_count = Hash.new(0)
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+    
+end
+
+lucifer = Song.new("Lucifer", "Jay-z", "Rap")
+problems = Song.new("99 Problems", "Jay-Z", "rap")
+hit = Song.new("hit me baby one more time", "Brittany Spears", "pop")
+
+
+# binding.pry
